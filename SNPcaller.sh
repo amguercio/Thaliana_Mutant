7313a64a5fd0ce31ae94ffa5e0ed9f518da0b788
Angelica Guercio <30634732+amiriamguercio@users.noreply.github.com>
2018-07-23 11:04:43 -0700

    Update and rename SNP_calling.sh to SNPcaller.sh

---
+++ SNPcaller.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --mem-per-cpu=40G
+#SBATCH --time=2-00:15:00     # 2 days and 15 minutes
+#SBATCH --output=snpping.stdout
+#SBATCH --mail-user=guercioa@ucr.edu
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="SNPping"
+#SBATCH -p koeniglab
+
+
+##	##SNP Calling with GATK##	##
+
+
+## Need:
+##realigned (no dup) .bam files (with path) as $1 and $2
+NAME = $(basename "$1" | cut -d_ -f1)
+
+#basename takes away path and file extension
+#then pipe into cut to take away everything after the _ delimiter; but only of the first line of the file
+
+
+
+module load gatk/3.7
+
+java -jar /opt/linux/centos/7.x/x86_64/pkgs/gatk/3.7/GenomeAnalysisTK.jar \
+-nt 10 \
+-T UnifiedGenotyper \
+-R /rhome/guercioa/bigdata/genomes/A_thaliana/thaliana_index.fa \
+-I $1 \
+-I $2 \
+-o rawSNPs.vcf \
+-glm BOTH	
+
+#both calls both SNPs and indels
+

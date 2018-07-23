e53465a267e53dd112ebf549ea71565696b4227a
Angelica Guercio <30634732+amiriamguercio@users.noreply.github.com>
2018-07-23 11:01:13 -0700

    Update Indexing_Reference.sh

--- Indexing_Reference.sh
+++ Indexing_Reference.sh
@@ -4,10 +4,10 @@
 #SBATCH --ntasks=10
 #SBATCH --mem-per-cpu=40G
 #SBATCH --time=2-00:15:00     # 2 days and 15 minutes
-#SBATCH --output=my.stdout
+#SBATCH --output=ref_index.stdout
 #SBATCH --mail-user=guercioa@ucr.edu
 #SBATCH --mail-type=ALL
-#SBATCH --job-name="dl"
+#SBATCH --job-name="indexing"
 #SBATCH -p koeniglab
 
 
@@ -22,8 +22,8 @@
 module load bwa/0.7.12
 
 bwa index \
--a bwtsw /rhome/guercioa/bigdata/thaliana_JGCRZ/thaliana_index/thaliana_index.fa \
--p /rhome/guercioa/bigdata/thaliana_JGCRZ/thaliana_index
+-a bwtsw /rhome/guercioa/bigdata/genomes/A_thaliana/thaliana_index.fa \
+-p /rhome/guercioa/bigdata/genomes/A_thaliana
 
 
 
@@ -33,14 +33,14 @@ bwa index \
 module load picard/2.10.0
 
 java -jar /opt/linux/centos/7.x/x86_64/pkgs/picard/2.10.0/lib/picard.jar CreateSequenceDictionary \
-R= /rhome/guercioa/bigdata/thaliana_JGCRZ/thaliana_index/thaliana_index.fa \
-O= /rhome/guercioa/bigdata/thaliana_JGCRZ/thaliana_index/thaliana_index.dict  
+R= /rhome/guercioa/bigdata/genomes/A_thaliana/thaliana_index.fa \
+O= /rhome/guercioa/bigdata/genomes/A_thaliana/thaliana_index.dict  
 
 
 module load samtools/1.4.1
 
 
-samtools faidx /rhome/guercioa/bigdata/thaliana_JGCRZ/thaliana_index/thaliana_index.fa
+samtools faidx /rhome/guercioa/bigdata/genomes/A_thaliana/thaliana_index.fa
 
 
 
